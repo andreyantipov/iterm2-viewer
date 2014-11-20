f9ae8ff30beff1eb61e9b04ee7e7c8e5663ea0fb
Andrey <andrey@antipov.me>
2014-11-20 17:35:55 +0300

    travis disable cache bundler

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
-require "minitest/autorun"
+require 'minitest/autorun'
 require 'minitest/unit'
 require 'minitest/pride'
-require "codeclimate-test-reporter"
 require File.expand_path('../../lib/iterm2/viewer.rb', __FILE__)
+
+require 'codeclimate-test-reporter'
 
 CodeClimate::TestReporter.start
