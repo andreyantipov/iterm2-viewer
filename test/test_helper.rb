b458743dccc70468acee14f8707da1b7b138b053
Andrey <andrey@antipov.me>
2014-11-20 16:56:19 +0300

    travis + codeclimate = test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,4 +2,7 @@ require "minitest/autorun"
 require 'minitest/unit'
 require 'minitest/pride'
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require File.expand_path('../../lib/iterm2/viewer.rb', __FILE__)
