4f9007ebe39d322a5880a542c7ab4a2c900944b6
Andrey <andrey@antipov.me>
2014-11-20 17:47:00 +0300

    update to 0.0.5 /n fixes, check version command -v or --version

--- lib/iterm2/viewer/render.rb
+++ lib/iterm2/viewer/render.rb
@@ -4,7 +4,7 @@ module Iterm2
     # @param object [Iterm2::Viewer::Entity]
     class Render
       def initialize(object)
-        raise ArgumentError, "Incorrect object" unless object.is_a? Iterm2::Viewer::Media
+        raise ArgumentError, "incorrect media file" unless object.is_a? Iterm2::Viewer::Media
         # @see  http://iterm2.com/images.html
         # @note based on https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat
         puts "\033]1337;File=;inline=1:#{object.data}\a\n"
--- lib/iterm2/viewer/version.rb
+++ lib/iterm2/viewer/version.rb
@@ -1,5 +1,5 @@
 module Iterm2
   module Viewer
-    VERSION = '0.0.4'
+    VERSION = '0.0.5'
   end
 end
